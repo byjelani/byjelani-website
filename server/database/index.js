0fbfb32d6dc9974cd789384159e7e8f775e51fc0
Jelani Hankins <Jelani.Hankins@ge.com>
2018-09-20 14:40:07 -0500

    (add) collections / orders

--- server/database/index.js
+++ server/database/index.js
@@ -14,6 +14,7 @@ const userSchema = mongoose.Schema({
   recentlyViewed: [String],
   totalSpent: Number,
   shippingAddress: String,
+  email: String,
 });
 
 const User = mongoose.model('User', userSchema);
@@ -41,5 +42,29 @@ const itemSchema = mongoose.Schema({
 
 const Item = mongoose.model('Item', itemSchema);
 
+const jCollectionSchema = mongoose.Schema({
+  name: String,
+  releaseDate: Date,
+  description: String,
+  // total sales of collection
+  totalSales: number,
+  images: [String],
+  lookBook: {}
+});
+
+const JCollection = mongoose.model('JCollection', JCollection);
+
+const orderSchema = mongoose.Schema({
+  customerEmail: String,
+  shippingAddress: String,
+  price: String,
+  items: [String],
+  status: String,
+})
+
+const Order = mongoose.model('Order', orderSchema);
+
 module.exports.Item = Item;
 module.exports.User = User;
+module.exports.JCollection = JCollection;
+module.exports.Order = Order;
