4a4f9ea8964c3b3c2a4ca01068802efe4e70616b
Jelani Hankins <Jelani.Hankins@ge.com>
2018-09-21 15:59:40 -0500

    (add) initial routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AccountpageComponent } from './accountpage/accountpage.component';
+import { CollectionspageComponent } from './collectionspage/collectionspage.component';
+import { TermsandconditionsComponent } from './termsandconditions/termsandconditions.component';
+import { ShopallComponent } from './shopall/shopall.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserService } from './services/user/user.service';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'account', component: AccountpageComponent},
+  { path: 'shop', component: ShopallComponent },
+  { path: 'shopall', component: ShopallComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccountpageComponent,
+    CollectionspageComponent,
+    TermsandconditionsComponent,
+    ShopallComponent,
+    AboutpageComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
-  providers: [],
+  providers: [ UserService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
